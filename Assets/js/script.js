3543de290fc12bab0cf474e3c230b7d7723f3da7
Annalise Tsalikis <annalisetsalikis@gmail.com>
2023-07-14 16:01:20 +1000

    updated JS code to make previous search btns, updated bootstrap for cards, final code, updated readme

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,15 +14,22 @@ $(document).ready(function () {
 
 function handleSearchSubmit(event) {
     event.preventDefault();
+    forecastEl.empty();
 
     //var searchInputVal = document.querySelector('#search-input');
-
     if (!searchInputVal) {
         console.error('You need a search input value!');
         return;
     }
     var city = searchInputVal.value.trim();
     var citiesList = JSON.parse(localStorage.getItem("previousCities"));
+
+    // Check if the city already exists in the list
+    if (citiesList && citiesList.includes(city)) {
+        // Remove the existing city from the list
+        citiesList = citiesList.filter(item => item !== city);
+    }
+
     citiesList.reverse();
     citiesList.push(city);
     citiesList.reverse();
@@ -31,6 +38,7 @@ function handleSearchSubmit(event) {
     getWeatherData(city);
 }
 
+//function to call API and pull city information for forecast and current weather information
 async function getWeatherData(cityName){
     let coordByLocation = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apikey}`;
     let coordForCity = await fetch(coordByLocation);
@@ -41,7 +49,9 @@ async function getWeatherData(cityName){
     let forecastApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
     let forecast = await fetch(forecastApi);
     let responsefc = await forecast.json();
+
     for (let i = 0; i <= 5; i++) {
+        //first statement i=0 refers to current day information for top box
         if (i === 0){
             var daycount = 0;
             let responsetime = responsefc['list'][daycount]['dt'];
@@ -49,7 +59,6 @@ async function getWeatherData(cityName){
             var daywind = responsefc['list'][daycount]['wind']['speed'];
             var dayhum = responsefc['list'][daycount]['main']['humidity'];
             var dayicon = responsefc['list'][daycount]['weather'][0]['icon'];
-
             currentInfoEl.children('h2').text(cityName + ' ' + dayjs.utc(responsetime * 1000).format(`(DD/MM/YYYY)`));
             currentInfoEl.children('img').attr('src', `https://openweathermap.org/img/wn/${dayicon}@2x.png`);
             currentInfoEl.children('p').eq(0).text('Temp: ' + daytemp + "ºC");
@@ -69,11 +78,11 @@ async function getWeatherData(cityName){
             forcastCardEl.children('div').children('p').eq(1).text('Wind: ' + daywind + 'km/h');
             forcastCardEl.children('div').children('p').eq(2).text('Humidity: ' + dayhum + '%');
             forecastEl.append(forcastCardEl.clone());
-        }
-        
+        } 
     }
 }
 
+//function to pull searches from local storage
 function getPreviousSearches() {
     if ((localStorage.getItem("previousCities"))===null) {
         var citiesList = [];
@@ -81,16 +90,20 @@ function getPreviousSearches() {
     } else {
         var citiesList = JSON.parse(localStorage.getItem("previousCities"));
     }
-    $("#sidebar ul li").empty();
+
+    //clear searches before appending to avoid duplicates
+    $("#sidebar ul").empty();
+
     for (let i = 0; i < citiesList.length; i++) {
-        $("#sidebar ul").append(`<li>${citiesList[i]}</li>`);
+        $("#sidebar ul").append(`<button>${citiesList[i]}</button>`);
     }
 }
 
 getPreviousSearches();
 searchBoxEl.addEventListener('submit', handleSearchSubmit);
 
-$('li').click(function(event) {
+//function handling when user clicks previous searches
+$(document).on('click', 'button', function(event) {
     var text = jQuery(this).text();
     forecastEl.empty();
     getWeatherData(text);
--- README.md
+++ README.md
@@ -6,15 +6,14 @@ A link to the deployed application can be found here:
 
 ## Description
 
-This week's Challenge required 
+This week's Challenge required me to build a weather application leveraging Open Weather API's. The application retrieves weather data for cities. It displays the current weather for the day in the city searched, as well as a 5-day forecast. As part of this forecast it shows the temperature in celcius, the wind, humidity as well as weather icon. To do this, the application first needs to pull the latitude and longtitude codes for the searched city, and then feeds this into the next api which pulls the data. The application also saves recent searches to local storage and displays them for the user to click on and revisit the weather data for that location.
 
-
-The Github Repository can be found here: 
+The Github Repository can be found here: https://github.com/annalisedev/weatherdashboard 
 
 ## Screenshot
 
 A screenshot of the deployed application can be found in assets:
-![ScreenshotofApplication](./assets/images/XX.png)
+![ScreenshotofApplication](./assets/images/ScreenshotWeatherApp.png)
 
 ## Support
 
@@ -22,3 +21,6 @@ Some external sources that supported me to understand the task and write the cod
 
 Add object from Local Storage to List on Page http://www.java2s.com/example/javascript/jquery/loop-through-an-array-and-add-every-object-to-ul-as-an-li.html
 
+JSON Formatting https://jsonformatter.org/json-pretty-print 
+
+Date formatting with Unix https://day.js.org/docs/en/parse/unix-timestamp 
